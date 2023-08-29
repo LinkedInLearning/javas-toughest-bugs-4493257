2e13dca9e2cb799355094a48568c597fbe174dc2
Kathryn Hodge <blondiebytes@gmail.com>
2023-08-29 11:43:17 +0000

    Re-add statement

--- Ch 1/01_01/Main.java
+++ Ch 1/01_01/Main.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
     int x = 5;
     while (x > 0) {
+      System.out.println("in loop");
     }
 
     System.out.println("out of loop");
