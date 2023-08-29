92b3fca37938530ea754d714decdd250ab4f8e28
Kathryn Hodge <blondiebytes@gmail.com>
2023-08-29 13:05:14 +0000

    Set up starter code 02_02

--- Ch 2/02_01/02_01e/Main.java
+++ Ch 2/02_01/02_01e/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> listy = null;
+    ArrayList<Integer> listy;
     listy.add(0);
   }
 }
---
+++ Ch 2/02_02/02_02b/model/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+  public static void main(String[] args) {
+    User user = new User("Kathryn");
+
+    String city = user.getAddress().getCity(); // Potential Null Pointer Exception!
+    System.out.println("City: " + city);
+  }
+}
+
+class User {
+  private String name;
+  private Address address;
+
+  public User(String name) {
+    this.name = name;
+  }
+
+  public Address getAddress() {
+    return this.address;
+  }
+
+  public void setAddress(Address address) {
+    this.address = address;
+  }
+}
+
+class Address {
+  private String city;
+
+  public Address(String city) {
+    this.city = city;
+  }
+
+  public String getCity() {
+    return city;
+  }
+}
