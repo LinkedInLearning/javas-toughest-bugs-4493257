2db7a4f8b2c31821b5c1654ab9451510fc8e830d
Kathryn Hodge <blondiebytes@gmail.com>
2023-08-31 21:23:12 +0000

    Updating

--- Ch 6/06_02/06_02e/StockTradingSystem.java
+++ Ch 6/06_02/06_02e/StockTradingSystem.java
@@ -2,7 +2,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class StockTradingSystem {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         ExecutorService threadPool = Executors.newFixedThreadPool(5);
 
         for (int i = 0; i < 10; i++) {
